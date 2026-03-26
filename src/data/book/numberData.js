8537f68f215d7c074a065860086b52e90bc65dd3
Crashyet <adhitya.pan@gmail.com>
2026-03-26 20:04:50 +0700

    add asset video

--- src/data/book/numberData.js
+++ src/data/book/numberData.js
@@ -15,21 +15,37 @@ import sembilan from '@/assets/book/number/9.png'
 // import 1000000000 from '../../../assets/book/number/1000000000.png'
 // import 1000000000000 from '../../../assets/book/number/1000000000000.png'
 
+import vid_1 from '@/assets/book/number/videos/01.webm'
+import vid_2 from '@/assets/book/number/videos/02.webm'
+import vid_3 from '@/assets/book/number/videos/03.webm'
+import vid_4 from '@/assets/book/number/videos/04.webm'
+import vid_5 from '@/assets/book/number/videos/05.webm'
+import vid_6 from '@/assets/book/number/videos/06.webm'
+import vid_7 from '@/assets/book/number/videos/07.webm'
+import vid_8 from '@/assets/book/number/videos/08.webm'
+import vid_9 from '@/assets/book/number/videos/09.webm'
+import puluhan from '@/assets/book/number/videos/puluhan.webm'
+import ratusan from '@/assets/book/number/videos/ratusan.webm'
+import ribuan from '@/assets/book/number/videos/ribuan.webm'
+import jutaan from '@/assets/book/number/videos/jutaan.webm'
+import miliaran from '@/assets/book/number/videos/miliyar.webm'
+import triliunan from '@/assets/book/number/videos/triliun.webm'
+
 export const numberData = [
-  { id: 1, title: 'Nol', image: nol, description: 'Angka 0 dilakukan dengan menggenggam tangan dan ibu jari berada di samping jari lainnya.' },
-  { id: 2, title: 'Satu', image: satu, description: 'Angka 1 dilakukan dengan menegakkan keempat jari dan melipat ibu jari ke dalam telapak tangan.' },
-  { id: 3, title: 'Dua', image: dua, description: 'Angka 2 dilakukan dengan melengkungkan seluruh jari membentuk huruf C dari samping.' },
-  { id: 4, title: 'Tiga', image: tiga, description: 'Angka 3 dilakukan dengan menegakkan jari telunjuk, sementara jari lainnya ditekuk bersama ibu jari.' },
-  { id: 5, title: 'Empat', image: empat, description: 'Angka 4 dilakukan dengan menekuk keempat jari ke arah telapak tangan dan ibu jari menyentuh ujung jari.' },
-  { id: 6, title: 'Lima', image: lima, description: 'Angka 5 dilakukan dengan menyatukan ujung jari telunjuk dan ibu jari membentuk lingkaran, sementara jari lain tegak.' },
-  { id: 7, title: 'Enam', image: enam, description: 'Angka 6 dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
-  { id: 8, title: 'Tujuh', image: tujuh, description: 'Angka 7 dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
-  { id: 9, title: 'Delapan', image: delapan, description: 'Angka 8 dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
-  { id: 10, title: 'Sembilan', image: sembilan, description: 'Angka 9 dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
-  { id: 11, title: 'Puluhan', image: null, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
-  { id: 12, title: 'Ratusan', image: null, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
-  { id: 13, title: 'Ribuan', image: null, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
-  { id: 14, title: 'Jutaan', image: null, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
-  { id: 15, title: 'Miliar', image: null, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
-  { id: 16, title: 'Triliun', image: null, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
+  { id: 1, title: 'Nol', image: nol, video: null, description: 'Angka 0 dilakukan dengan menggenggam tangan dan ibu jari berada di samping jari lainnya.' },
+  { id: 2, title: 'Satu', image: satu, video: vid_1, description: 'Angka 1 dilakukan dengan menegakkan keempat jari dan melipat ibu jari ke dalam telapak tangan.' },
+  { id: 3, title: 'Dua', image: dua, video: vid_2, description: 'Angka 2 dilakukan dengan melengkungkan seluruh jari membentuk huruf C dari samping.' },
+  { id: 4, title: 'Tiga', image: tiga, video: vid_3, description: 'Angka 3 dilakukan dengan menegakkan jari telunjuk, sementara jari lainnya ditekuk bersama ibu jari.' },
+  { id: 5, title: 'Empat', image: empat, video: vid_4, description: 'Angka 4 dilakukan dengan menekuk keempat jari ke arah telapak tangan dan ibu jari menyentuh ujung jari.' },
+  { id: 6, title: 'Lima', image: lima, video: vid_5, description: 'Angka 5 dilakukan dengan menyatukan ujung jari telunjuk dan ibu jari membentuk lingkaran, sementara jari lain tegak.' },
+  { id: 7, title: 'Enam', image: enam, video: vid_6, description: 'Angka 6 dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
+  { id: 8, title: 'Tujuh', image: tujuh, video: vid_7, description: 'Angka 7 dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
+  { id: 9, title: 'Delapan', image: delapan, video: vid_8, description: 'Angka 8 dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
+  { id: 10, title: 'Sembilan', image: sembilan, video: vid_9, description: 'Angka 9 dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
+  { id: 11, title: 'Puluhan', image: null, video: puluhan, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
+  { id: 12, title: 'Ratusan', image: null, video: ratusan, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
+  { id: 13, title: 'Ribuan', image: null, video: ribuan, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
+  { id: 14, title: 'Jutaan', image: null, video: jutaan, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
+  { id: 15, title: 'Miliar', image: null, video: miliaran, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
+  { id: 16, title: 'Triliun', image: null, video: triliunan, description: 'Huruf G dilakukan dengan menjulurkan ibu jari dan telunjuk ke arah depan.' },
 ]
