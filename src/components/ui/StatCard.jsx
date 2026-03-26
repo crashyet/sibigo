b4d5dc8e343ff08baa3ed59a8e701fc9ae50d1d1
Crashyet <adhitya.pan@gmail.com>
2026-03-26 17:19:59 +0700

    refactor: landing page

--- src/components/ui/StatCard.jsx
+++ src/components/ui/StatCard.jsx
@@ -9,7 +9,7 @@ import React from 'react'
  */
 const StatCard = ({ value, label }) => {
   return (
-    <div className="px-6 lg:w-[320px] md:px-9 py-5 md:py-10 lg:py-14 bg-[#777BDB] rounded-2xl md:rounded-3xl">
+    <div className="flex-1 px-4 md:px-6 lg:px-8 py-5 md:py-8 lg:py-12 bg-[#777BDB] rounded-2xl md:rounded-3xl">
       <h3 className="font-pjs font-bold text-2xl md:text-3xl lg:text-4xl text-center mb-2 md:mb-4">
         {value}
       </h3>
